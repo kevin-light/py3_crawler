94af3ab595e1aac5998676c94880762bc1e50938
kevin-light <895103675@qq.com>
2017-07-05 08:49:09 +0800

    py3-crawler--taobao

--- Progects/lesson3/TbMeishi/spider.py
+++ Progects/lesson3/TbMeishi/spider.py
@@ -1,17 +1,25 @@
 import re
+
+import pymongo
+from pyquery import PyQuery as pq
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pyquery import PyQuery as pq
+from selenium.webdriver.support.ui import WebDriverWait
 from config import *
 
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB]
 
-browser = webdriver.Chrome()
+# browser = webdriver.Chrome()
+browser = webdriver.PhantomJS(service_args=SERVICE_ARGS)
 wait = WebDriverWait(browser,10)
 
+browser.set_window_size(1400,900)
+
 def search():
+    print('正在搜索')
     try:
         browser.get('https://www.taobao.com')
         input = wait.until(
@@ -19,7 +27,7 @@ def search():
         )
         submit = wait.until(EC.element_to_be_clickable(
             (By.CSS_SELECTOR,'#J_TSearchForm > div.search-button > button')))
-        input.send_keys('美食')
+        input.send_keys(KEYWORD)
         submit.click()
         total = wait.until(EC.presence_of_element_located(
             (By.CSS_SELECTOR,'#mainsrp-pager > div > div > div > div.total')))
@@ -29,6 +37,7 @@ def search():
         return search()
 
 def next_page(page_number):
+    print('当前翻页：', page_number)
     try:
         input = wait.until(EC.presence_of_element_located(
             (By.CSS_SELECTOR, "#mainsrp-pager > div > div > div > div.form > input")))
@@ -61,18 +70,25 @@ def get_products():
             'location': item.find('.location').text()
         }
         print(product)
-    # for item in items:
-    #     product = {'image': item.find('.pic .img').attr('src'), 'price': item.find('.price').text(),
-    #         'deal': item.find('.deal-cnt').text()[:-3], 'title': item.find('.title').text(),
-    #         'shop': item.find('.shop').text(), 'location': item.find('.location').text()}
-    #     print(product)
+        save_to_mongo(product)
+def save_to_mongo(result):
+    try:
+        if db[MONGO_TABLE].insert(result):
+            print('存储到mongodb成功',result)
+    except Exception:
+        print('存储到mongodb失败',result)
 
 def main():
-    total = search()
-    total = int(re.compile('(\d+)').search(total).group(1))
-    for i in range(2, total+1):
+    try:
+        total = search()
+        total = int(re.compile('(\d+)').search(total).group(1))
+        for i in range(2, total+1):
 
-        next_page(i)
+            next_page(i)
+    except Exception:
+        print('出问题了')
+    finally:
+        browser.close()
 
 if __name__ == '__main__':
     main()
